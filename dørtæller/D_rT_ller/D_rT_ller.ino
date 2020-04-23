4b795c5a46ad04a684ab50ef35323ef51185193e
P00lord <37065834+P00lord@users.noreply.github.com>
2020-04-23 14:55:26 +0200

    måske et eller andet

--- dørtæller/D_rT_ller/D_rT_ller.ino
+++ dørtæller/D_rT_ller/D_rT_ller.ino
@@ -1,27 +1,16 @@
 #include <WiFi.h>
-const char* ssid = "UserName";
-const char* pass = "PassWord";
+#include <HardwareSerial.h>
 
-#define sensor1 33
-#define sensor2 32
-int a[10];
-int b[10];
-int display1;
-int display2;
-int aTotal;
-int bTotal;
-int averageSens1;
-int averageSens2;
+#define RXD2 16
+#define TXD2 17
+//HardwareSerial Serial1(1);
+
+const char* ssid = "esp32";
+const char* pass = "12345679";
+
+String portRead;
+
 int intruders;
-boolean armed;
-boolean in;
-boolean in2;
-boolean inThrough;
-boolean out;
-boolean out2;
-boolean outThrough;
-boolean middleIn;
-boolean middleOut;
 
 int lokaleNr = 1;
 int lastIntruders;
@@ -29,7 +18,7 @@ String lN;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  delay(10);
+/*  delay(10);
   WiFi.begin(ssid,pass);
   Serial.print("Connecting.");
     while(WiFi.status() != WL_CONNECTED) {
@@ -40,119 +29,22 @@ void setup() {
     Serial.print("WiFi connected - IP address: ");
     Serial.println(WiFi.localIP());
     delay(500);
-  analogSetWidth(10);
   lastIntruders = intruders;
   lN = String(lokaleNr);
   Serial.println(lN);
+  */
+  Serial1.begin(9600, SERIAL_8N1,RXD2,TXD2);
 }
 
 void loop() {
-//Læs på sensoren 10 gange og læg de 10 målinger i et array
-for(int i = 0;i<11;i++){
-    display1 = analogRead(sensor1);
-    a[i] = display1;
+  if(Serial1.available()){
+    String incommingData = Serial1.readString();
+    int komma = incommingData.indexOf(",");
+    String incommingIntruders = incommingData.substring(0,komma);
+    Serial.print(incommingIntruders);
+    //Serial.print(fs);
   }
-//Læs på sensoren 10 gange og læg de 10 målinger i et array
-for(int i = 0;i<11;i++){
-    display2 = analogRead(sensor2);
-    b[i] = display2;
-}
-
-//Læg værdierne af 10 målinger sammen
-aTotal = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9];
-//Lav et gennemsnit af de 10 målinger 
-averageSens1 = aTotal/10;
-//Læg værdierne af 10 målinger sammen
-bTotal = b[0]+b[1]+b[2]+b[3]+b[4]+b[5]+b[6]+b[7]+b[8]+b[9];
-//Lav et gennemsnit af de 10 målinger
-averageSens2 = bTotal/10;
-
-//Er døråbningen fri?
-if(averageSens1 <= 200 && averageSens2 <= 200){
-  armed = true;
-}
-
-if(averageSens1 > 200){
-    in = true;
-  }
-if(in == true){
-  if(averageSens2 > 200){
-      middleIn = true;
-    }
-  else if(averageSens1 <= 200){
-    in = false;
-  }
-}  
-if(middleIn == true){
-  if(averageSens1 < 200){
-    in2 = true;
-    in = false;
-    middleIn = false;
-    }
-   else if(averageSens2 < 200){
-   in2 = false;
-   in = true;
-   middleIn = false;
-   }
-}
-if(in2 == true){
-  if(averageSens2 < 200){
-    in2 = false;
-    inThrough = true;
-  }
-  if(averageSens1 > 200){
-    middleIn = true;
-  }
-}
-
-while(inThrough == true){
-  intruders++;
-  inThrough = false;
-  break;
-}
-
-if(averageSens2 > 200){
-    out = true;
-  }
-if(out == true){
-  if(averageSens1 > 200){
-      middleOut = true;
-    }
-  else if(averageSens2 <= 200){
-    out = false;
-  }
-}  
-if(middleOut == true){
-  if(averageSens2 < 200){
-    out2 = true;
-    out = false;
-    middleOut = false;
-    }
-   else if(averageSens1 < 200){
-   out2 = false;
-   out = true;
-   middleOut = false;
-   }
-}
-if(out2 == true){
-  if(averageSens1 < 200){
-    out2 = false;
-    outThrough = true;
-  }
-  if(averageSens2 > 200){
-    middleOut = true;
-  }
-}
-
-while(outThrough == true){
-  intruders--;
-  if(intruders <= 0){
-    intruders = 0;
-  }
-  outThrough = false;
-  break;
-  }
-
+  /*
   if(intruders != lastIntruders){
     const uint16_t port = 7000;
     WiFiClient client;
@@ -164,6 +56,7 @@ while(outThrough == true){
      lastIntruders = intruders;
     }
   }
+  */
   //Serial.println(intruders);
   //Serial.println(analogRead(sensor2));
 //delay(100);
